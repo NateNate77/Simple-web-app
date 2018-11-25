80d7526126048806b5f7b1855b479d232fd2db77
Nate_Hate <chekireva.nate@gmail.com>
2018-11-25 05:23:20 +0500

    страницы html деревьев

---
+++ src/main/java/simplewebapp/domain/CompanyTree.java
@@ -0,0 +1,11 @@
+package simplewebapp.domain;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Admin on 25.11.2018.
+ */
+public class CompanyTree {
+    public Company company;
+    public ArrayList<CompanyTree> companyTrees = new ArrayList<>();
+}
---
+++ src/main/java/simplewebapp/domain/UserTree.java
@@ -0,0 +1,8 @@
+package simplewebapp.domain;
+
+import java.util.ArrayList;
+
+public class UserTree{
+        public User user;
+public ArrayList<UserTree> userTrees = new ArrayList<>();
+}
